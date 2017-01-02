516dfd27913daedebcfb592a85504192b489a248
weichaojie <18971302695@189.cn>
2017-01-02 22:30:44 +0800

    commit html call js file

--- firsstweb/js/FirstJavaScript.js
+++ firsstweb/js/FirstJavaScript.js
@@ -1,4 +1,10 @@
 
+alert("This js file was called")
+
+function myFunction() {
+    document.getElementById("demo").innerHTML="my first js function";
+}
+
 function displayDate() {
     document.getElementById("demo").innerHTML=Date();
 }
